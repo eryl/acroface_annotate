5364808833d29848a29797f0d10f6c1f40f45ae9
Erik Ylipää <erik.ylipaa@gmail.com>
2024-04-30 09:38:14 +0200

    Added alpha composite

--- encrypt_data_no_gui.py
+++ encrypt_data_no_gui.py
@@ -14,10 +14,52 @@ from typing import Tuple
 from PIL import Image, UnidentifiedImageError, ImageOps
 
 from tqdm import tqdm
+import numpy as np
 
 import aes
 
 target_size = (512, 512)
+
+def alpha_composite(front, back):
+    """Alpha composite two RGBA images.
+
+    Source: http://stackoverflow.com/a/9166671/284318
+
+    Keyword Arguments:
+    front -- PIL RGBA Image object
+    back -- PIL RGBA Image object
+
+    """
+    front = np.asarray(front)
+    back = np.asarray(back)
+    result = np.empty(front.shape, dtype='float')
+    alpha = np.index_exp[:, :, 3:]
+    rgb = np.index_exp[:, :, :3]
+    falpha = front[alpha] / 255.0
+    balpha = back[alpha] / 255.0
+    result[alpha] = falpha + balpha * (1 - falpha)
+    old_setting = np.seterr(invalid='ignore')
+    result[rgb] = (front[rgb] * falpha + back[rgb] * balpha * (1 - falpha)) / result[alpha]
+    np.seterr(**old_setting)
+    result[alpha] *= 255
+    np.clip(result, 0, 255)
+    # astype('uint8') maps np.nan and np.inf to 0
+    result = result.astype('uint8')
+    result = Image.fromarray(result, 'RGBA')
+    return result
+
+
+def alpha_composite_with_color(image, color=(255, 255, 255)):
+    """Alpha composite an RGBA image with a single color image of the
+    specified color and the same size as the original image.
+
+    Keyword Arguments:
+    image -- PIL RGBA Image object
+    color -- Tuple r, g, b (default 255, 255, 255)
+
+    """
+    back = Image.new('RGBA', size=image.size, color=color + (255,))
+    return alpha_composite(image, back)
 
 # For some reason this was missing from the ImageOps bundled with PIL in psychopy, including it from the sources here
 def cover(
@@ -53,7 +95,8 @@ def cover(
 def encrypt_file(work_package):
     image_path, output_directory, bytes_password, format, = work_package
     #print(f"Encrypting {path}")
-    pil_image = Image.open(image_path).convert("RGB")
+    pil_image = Image.open(image_path)
+    pil_image = alpha_composite_with_color(pil_image).convert('RGB')
     pil_image = cover(pil_image, target_size)
     output_buffer = io.BytesIO()
     pil_image.save(output_buffer, format=format)
